a95710fccf9449b22a12725e917e3d9da47d5dba
NewAryabro <138558338+NewAryabro@users.noreply.github.com>
2023-08-14 23:47:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,9 +21,9 @@ def is_enabled(value, default):
 PORT = environ.get("PORT", "8080")
 WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
 SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ('API_ID') "")) 
-API_HASH = environ('API_HASH') "")) 
-BOT_TOKEN = environ('BOT_TOKEN') "")) 
+API_ID = int(environ('API_ID') )) 
+API_HASH = environ('API_HASH') )) 
+BOT_TOKEN = environ('BOT_TOKEN') )) 
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -42,8 +42,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "")
+DATABASE_URI = environ.get('DATABASE_URI', )
+DATABASE_NAME = environ.get('DATABASE_NAME', )
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 #maximum search result buttos count in number#
